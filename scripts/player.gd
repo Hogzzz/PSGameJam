2a97243f327b4f865ba990695604f02a13924031
TosknDev <thomaspindroh1@gmail.com>
2024-07-25 23:22:38 -0400

    player has higher jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
 var enemy_inattack_range = false
 var enemy_cooldown = true
 var health = 100
