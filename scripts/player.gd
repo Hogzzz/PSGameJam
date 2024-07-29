c992c4ccb1fda3c9a8b43c35883031c9314c7dd9
Hogzzz <iankichurchak@gmail.com>
2024-07-29 00:47:37 -0400

    Double Jump and Wall Jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -350.0
+const WALL_SLIDE_MAX_SPEED = 4
 var enemy_inattack_range = false
 var enemy_cooldown = true
 var health = 100
 var player_alive = true
-var attack_ip = false
+var amount_of_jumps = 1
+var is_wall_sliding = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var playerref = $"."
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var death = $Death
+@onready var jump_timer = $"Jump Timer"
+
 
 func _physics_process(delta):
-
-	# Death Actions
+	jump()
+	# Death Action
 	if health <=0:
 		player_alive = false
 		print("You Died!")
@@ -27,13 +30,18 @@ func _physics_process(delta):
 		death.start()
 
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
 		velocity.y += gravity * delta
+	elif !is_on_floor():
+		velocity.y += gravity * delta
+	elif is_on_wall() and !is_on_floor():
+		velocity.y = WALL_SLIDE_MAX_SPEED
+	
 	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+	#elif Input.is_action_just_pressed("jump") and 
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -47,13 +55,11 @@ func _physics_process(delta):
 	# Play anmiations
 	if is_on_floor():
 		if direction == 0:
-			if attack_ip == false:
-				animated_sprite.play("Idle")
+			animated_sprite.play("Idle")
 		else:
 			animated_sprite.play("Run")
 	else:
-		animated_sprite.play("Jump")
-		animated_sprite.play("Fall")
+			animated_sprite.play("Jump")
 
 	
 	# Apply movement
@@ -63,9 +69,17 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-
-func _on_timer_timeout():
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
-
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() :
+			if amount_of_jumps == 0:
+				amount_of_jumps = 1
+			velocity.y = JUMP_VELOCITY
+			print(amount_of_jumps)
+		elif !is_on_floor() and amount_of_jumps > 0:
+			velocity.y = JUMP_VELOCITY
+			amount_of_jumps -= 1
+			print(amount_of_jumps)
+		elif is_on_wall() and amount_of_jumps > 0:
+			velocity.y = JUMP_VELOCITY
+			amount_of_jumps -= 1
